61592a5bbae2b0b246ef546a84f7013944ca8aec
jazminkugler <jkugler@mail.austral.edu.ar>
2025-04-14 15:55:33 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,5 +1,5 @@
 def line():
-  A = float (input ("ingrese el coeficiente A: "))
+	A = float (input ("ingrese el coeficiente A: "))
 	B = float (input ("ingrese el coeficiente B: "))
 	X1 = float (input ("ingrese el coeficiente X1: "))
 	X2 = float (input ("ingrese el coeficiente X2: "))
