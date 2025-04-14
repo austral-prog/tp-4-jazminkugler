42765dd9f08b90b734926bea63658623c7c8b80b
jazminkugler <jkugler@mail.austral.edu.ar>
2025-04-14 15:59:14 -0300

    Update line.py

--- line.py
+++ line.py
@@ -3,10 +3,10 @@ def line():
 	B = float (input ("ingrese el coeficiente B: "))
 	X1 = float (input ("ingrese el coeficiente X1: "))
 	X2 = float (input ("ingrese el coeficiente X2: "))
-	print (f" El coeficinete A de su ecuacion de la recta es: {A}")
-	print (f" El coeficinete A de su ecuacion de la recta es: {B}")
-	print (f" El coeficinete A de su ecuacion de la recta es: {X1}")
-	print (f" El coeficinete A de su ecuacion de la recta es: {X2}")
+	print (f" El coeficinete A de su ecuación de la recta es: {A}")
+	print (f" El coeficinete A de su ecuación de la recta es: {B}")
+	print (f" El coeficinete A de su ecuación de la recta es: {X1}")
+	print (f" El coeficinete A de su ecuación de la recta es: {X2}")
 	print("\n")
 	print ("Para la siguinete ecuación:")
 	print(f"\t y= {A}X + {B}")
@@ -19,3 +19,9 @@ def line():
 	print ("\n")
 	distancia = ((X1-X2)**2+(Y1-Y2)**2)**(1/2)
 	print(f"La distancia entre ellos es:{distancia}")
+
+	año = int(input('Ingrese un año: '))
+	If ((año % 4 == 0) and (año % 100 != 0)):
+		print(f'El año {año} es bisiesto') or ((año % 4 == 0) and (año % 400 == 0))
+	else: 
+		print(f'el año {año} no es bisiesto')
